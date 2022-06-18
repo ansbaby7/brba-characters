77b4d72946421696a59ca2ffaae26f19e3a90737
Ans Baby <ansbaby7@gmail.com>
2022-06-19 00:47:39 +0530

    add link to home in header

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,21 +1,29 @@
 import Link from "next/link";
-import {ImGithub} from "react-icons/im"
+import { ImGithub } from "react-icons/im";
 
 const Header = () => {
   return (
     <div className="bg-stone-200  flex flex-col justify-between items-center sm:flex-row">
-        {/* bg-[#29773E] */}
-      <div className="flex items-center gap-x-4">
-        <img
-          src="https://brandlogos.net/wp-content/uploads/2012/10/breaking-bad-logo-vector.png"
-          className="w-16 h-16 ml-1 md:w-24 md:h-24 md:ml-4"
-        />
-        <span className="ml-4 text-slate-800 text-xl md:text-5xl uppercase font-bold">Characters</span>
-      </div>
+      {/* bg-[#29773E] */}
+      <Link href="/">
+        <a>
+          <div className="flex items-center gap-x-4">
+            <img
+              src="https://brandlogos.net/wp-content/uploads/2012/10/breaking-bad-logo-vector.png"
+              className="w-16 h-16 ml-1 md:w-24 md:h-24 md:ml-4"
+            />
+            <span className="ml-4 text-slate-800 text-xl md:text-5xl uppercase font-bold">
+              Characters
+            </span>
+          </div>
+        </a>
+      </Link>
       <Link href="https://github.com/ansbaby7/brba-characters">
-      <a className="">
-        <span className=""><ImGithub className="h-6 w-16 mt-4 mb-4 sm:mb-0"/></span>
-      </a>
+        <a className="">
+          <span className="">
+            <ImGithub className="h-6 w-16 mt-4 mb-4 sm:mb-0" />
+          </span>
+        </a>
       </Link>
     </div>
   );
